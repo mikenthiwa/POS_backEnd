3e8ddf27cb2a183353556c33b0d0a0d6af9f6bca
mike.nthiwa <mike.nthiwa@gmail.com>
2019-11-18 18:45:29 +0300

    ft(sockect.io)real time communication:  - Implement real-time communication between server and and client WIP

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,11 @@
 import http from 'http';
 import app from './app';
 import env from '../../environments';
+import io from 'socket.io';
 
 const server = http.createServer(app());
+
 const port = env.PORT;
+const socket = io(server);
+socket.on('connection', (socket) => console.log('connected', socket));
 server.listen(port, () => console.log(`Port ${port} is running`));
