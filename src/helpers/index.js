a453beae2dd8aa5323955d523013ffbe16241325
mike.nthiwa <mike.nthiwa@gmail.com>
2019-12-14 19:20:39 +0300

    refactore code

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import uniqid from 'uniqid';
+import models from '../db/models';
 
 
 export const jsonResponse = (res, status, success, message, data) => (
@@ -23,6 +24,54 @@ export const findCreateDB = (model, Product) => {
 	return model.Product.findOrCreate({
 		where: { ProductName },
 		defaults: {ProductId},
+	});
+};
+
+export default (io) => {
+	io.on('connection', async socket => {
+		const data = await findAllDb(models);
+		socket.emit('initialList', data);
+		socket.on('addProduct', async product => {
+			const newProduct = await findCreateDB(models, product);
+			if(newProduct[1]) {
+				const [ created ] = newProduct;
+				io.emit('productAdded', created.get({plain: true}));
+				socket.emit('productAddedSuccess', 'Successfully added');
+			}
+			else {
+				io.emit('productAdded', {conflictMessage: 'Product already exists'});
+			}
+		});
+		// Listen to update product action from client
+		socket.on('updateProduct', async productData => {
+			const { productId, ProductName } = productData;
+			const product = await models.Product.update(
+				{ProductName: ProductName},
+				{
+					where: {ProductId: productId},
+					returning: true,
+					plain: true,
+					raw: true,
+				},
+
+			);
+			const data = await findAllDb(models);
+			const updatedData = {products: data, product: product[1]};
+			io.emit('updatedProduct', updatedData);
+		});
+		// listen to delete product action from client
+		socket.on('deleteProduct', async productId => {
+			const product = await findItemDb(models, productId);
+			if(product){
+				await models.Product.destroy({
+					where: {
+						ProductId: productId
+					}
+				});
+				const data = await findAllDb(models);
+				io.emit('initialList', data);
+			}
+		});
 	});
 };
 
--- src/server/index.js
+++ src/server/index.js
@@ -2,56 +2,14 @@ import http from 'http';
 import app from './app';
 import env from '../environments';
 import Io from 'socket.io';
-import models from '../db/models';
-import { findAllDb, findCreateDB, findItemDb } from '../helpers';
+
+import socketConnection from '../helpers';
 
 const server = http.createServer(app());
 
 const port = env.PORT;
 export const io = Io(server);
-
-io.on('connection', async (socket) => {
-	const data = await findAllDb(models);
-	socket.emit('initialList', data);
-	socket.on('addProduct', async product => {
-		const newProduct = await findCreateDB(models, product);
-		if(newProduct[1]) {
-			const [ created ] = newProduct;
-			io.emit('productAdded', created.get({plain: true}));
-		}
-		else {
-			io.emit('productAdded', {conflictMessage: 'Product already exists'});
-		}
-	});
-	socket.on('updateProduct', async productData => {
-		const { productId, ProductName } = productData;
-		const product = await models.Product.update(
-			{ProductName: ProductName},
-			{
-				where: {ProductId: productId},
-				returning: true,
-				plain: true,
-				raw: true,
-			},
-
-		);
-		const data = await findAllDb(models);
-		const updatedData = {products: data, product: product[1]};
-		io.emit('updatedProduct', updatedData);
-	});
-	socket.on('deleteProduct', async productId => {
-		const product = await findItemDb(models, productId);
-		if(product){
-			await models.Product.destroy({
-				where: {
-					ProductId: productId
-				}
-			});
-			const data = await findAllDb(models);
-			io.emit('initialList', data);
-		}
-	});
-});
+socketConnection(io);
 
 server.listen(port, () => console.log(`Port ${port} is running`));
 
